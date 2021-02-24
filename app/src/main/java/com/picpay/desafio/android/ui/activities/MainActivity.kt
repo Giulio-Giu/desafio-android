d1fa4fbbd3131ec79b0572e881429e76f1f91d83
Giulio Machado <g.98.machado@hotmail.com>
2021-02-24 20:21:33 -0300

    Removendo imports não utilizados

--- app/src/main/java/com/picpay/desafio/android/ui/activities/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/activities/MainActivity.kt
@@ -6,19 +6,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.picpay.desafio.android.services.PicPayService
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.models.User
 import com.picpay.desafio.android.services.RetrofitInitializer
 import com.picpay.desafio.android.ui.adapters.UserListAdapter
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
